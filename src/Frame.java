55edbeac116a5f9a372ca8b07029c6e0d1ec34c3
Usman Baig <auopak8@gmail.com>
2015-07-23 21:06:18 -0400

    Variables

--- src/Frame.java
+++ src/Frame.java
@@ -1,15 +1,25 @@
+import java.awt.Dimension;
+
 import javax.swing.*;
 
 public class Frame extends JFrame {
-	public static void main(String args[]){
+	public static String title = "TowerDefense Alpha";
+	public static Dimension size = new Dimension(550, 650);
+	
+	public Frame(){
+		setTitle(title);
+		setSize(size);
+		setResizable(false);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 	}
-	
 	public void init(){
 		
 	}
 	
-	public Frame(){
-		
+	public static void main(String args[]){
+		Frame frame = new Frame();
 	}
+	
 }
