7451c79cbdb4ef2fe2658b7959430d1cd4cb337f
Usman Baig <auopak8@gmail.com>
2015-07-25 13:24:14 -0400

    screen

--- src/Frame.java
+++ src/Frame.java
@@ -1,4 +1,5 @@
 import java.awt.Dimension;
+import java.awt.GridLayout;
 
 import javax.swing.*;
 
@@ -17,6 +18,11 @@ public class Frame extends JFrame {
 		
 	}
 	public void init(){
+		setLayout(new GridLayout(1, 1, 0, 0));
+		
+		Screen screen = new Screen();
+		add(screen);
+		
 		setVisible(true);
 	}
 	
