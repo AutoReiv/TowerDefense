6207e9cec27ae74409440b04d19d0c5d2832ecfc
Usman Baig <auopak8@gmail.com>
2015-08-01 21:27:58 -0400

    1.2

--- src/Screen.java
+++ src/Screen.java
@@ -4,12 +4,16 @@ import javax.swing.*;
 public class Screen extends JPanel implements Runnable {
 	public Thread thread = new Thread(this);
 	
+	public static boolean isFirst = true;
+	
 	public Screen(){
 		thread.start();
 	}
 	
 	public void paintComponent(Graphics g){
 		
+		
+		g.clearRect(0, 0, getWidth(), getHeight());
 	}
 	
 	public static int fpsFrame = 0, fps = 1000000;
